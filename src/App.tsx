a6ba06c97b1873048776552c01c7afec0d11e83b
Shuyang <shuyang0129@gmail.com>
2023-03-28 22:17:17 +0800

    feat: reset to starting point

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,26 @@
-import DropdownMenu from 'components/DropdownMenu'
-import DropdownMenuItem from 'components/DropdownMenu/DropdownMenuItem'
-import { Fragment, useState, MouseEvent } from 'react'
+import { Fragment } from 'react'
 import { RxDotsVertical, RxExit, RxQuestionMarkCircled, RxRocket } from 'react-icons/rx'
 
 function App() {
-  const [anchorElement, setAnchorElement] = useState<HTMLElement | null>(null)
-  const isOpen = Boolean(anchorElement)
-
-  const handleClick = (event: MouseEvent<HTMLElement>) => {
-    setAnchorElement(isOpen ? null : event.currentTarget)
-  }
-
-  const handleClose = () => setAnchorElement(null)
-
   return (
     <Fragment>
-      <div style={{ width: '100%', height: '100vh', backgroundColor: 'transparent' }} />
-      <div style={{ width: '100%', height: '100vh', backgroundColor: 'lightcyan' }} />
-      <div style={{ width: '100%', height: '100vh', backgroundColor: 'lightgoldenrodyellow' }} />
-      <button className="button__icon fixed-center" onClick={handleClick}>
+      <button className="button__icon fixed-center">
         <RxDotsVertical className="icon" />
       </button>
-      <DropdownMenu
-        isOpen={isOpen}
-        anchorElement={anchorElement}
-        handleClose={handleClose}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      >
-        <DropdownMenuItem>
+      <div className="dropdown-menu__list" style={{ position: 'fixed' }}>
+        <div className="dropdown-menu__item">
           <RxRocket style={{ fontSize: '16px' }} />
           Get started guide
-        </DropdownMenuItem>
-        <DropdownMenuItem>
+        </div>
+        <div className="dropdown-menu__item">
           <RxQuestionMarkCircled style={{ fontSize: '16px' }} />
           Help center
-        </DropdownMenuItem>
-        <DropdownMenuItem>
+        </div>
+        <div className="dropdown-menu__item">
           <RxExit style={{ fontSize: '16px' }} />
           Sign out
-        </DropdownMenuItem>
-      </DropdownMenu>
+        </div>
+      </div>
     </Fragment>
   )
 }
